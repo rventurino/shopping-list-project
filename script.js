baf162684c42d7338ea190414a3b93f0064a6636
Ryan <ryan.venturino@gmail.com>
2023-06-28 08:29:55 -0400

    make list items removeable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearBtn = document.getElementById('clear')
 
 function addItem(e) {
     e.preventDefault()
@@ -16,7 +17,7 @@ function addItem(e) {
     //Create the list item
     const li = document.createElement('li')
     li.appendChild(document.createTextNode(newItem))
-    //create the button containing delete symbol
+    //create the button containing delete symbol via function call
     const button = createButton('remove-item btn-link text-red')
     //add button to li
     li.appendChild(button)
@@ -30,7 +31,7 @@ function addItem(e) {
 function createButton(classes){
     const button = document.createElement('button')
     button.className = classes
-    //create the icon
+    //create the icon via function call 
     const icon = createIcon('fa-solid fa-xmark')
     button.appendChild(icon)
     return button
@@ -41,5 +42,37 @@ function createIcon(classes){
     icon.className = classes
     return icon
 }
+
+//////////////////////////
+
+function removeItem(e) {
+    /*
+    //Demo
+    console.log(e.target)
+    e.target.remove();
+    //Behavior here doesn't work. This will log the X button and delete ONLY the X
+    */
+   if(e.target.parentElement.classList.contains('remove-item')){
+    //e.target = icon
+    //e.target.parentElement = button containing icon
+    //e.target.parentElement.parentElement = entire LI containing text, button, and icon
+    e.target.parentElement.parentElement.remove()
+
+   }
+}
+
+//////////////////////////////////
+
+function clearItems() {
+    while(itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
 //Event Listeners
+//event listener to add item by listening for submit event and calling addItem function
 itemForm.addEventListener('submit', addItem)
+//event listener to target the x button on the LI and trigger removal
+itemList.addEventListener('click', removeItem)
+//event listener to clear entire list by clicking clear all
+clearBtn.addEventListener('click', clearItems)
